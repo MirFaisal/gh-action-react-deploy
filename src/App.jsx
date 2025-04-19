977df461d640a5f7108dd5aeb9402cafc932223a
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-04-19 21:36:47 +0600

    fix: update heading in App component to reflect protective environment

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Deployment with Github Action</h1>
+      <h1>Deployment with Github Action with protective environment</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
